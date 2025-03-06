8efa9fa70d76de133f2d19c1102d3fd45e6b9248
jjosecodes <josejob123121@gmail.com>
2025-03-05 20:07:42 -0600

    loops

---
+++ basics /loops.py
@@ -0,0 +1,14 @@
+# while 
+class different_loops:
+    def WhileLoop(self):
+        counter = 1 
+        while counter <= 5:
+            print("hello world ", counter)
+            counter = counter + 1
+
+
+
+
+
+loop = different_loops()
+#loop.WhileLoop()
