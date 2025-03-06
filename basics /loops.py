11ac53a8bc61fc3bd272c68c17865c4e39c5dd09
jjosecodes <josejob123121@gmail.com>
2025-03-06 10:54:23 -0600

    for

--- basics /loops.py
+++ basics /loops.py
@@ -1,14 +1,22 @@
 # while 
 class different_loops:
-    def WhileLoop(self):
-        counter = 1 
-        while counter <= 5:
+    def WhileLoop(self ,counter: int = 1 ):
+
+        while counter <= 10:
             print("hello world ", counter)
             counter = counter + 1
+
+    def ForLoop(self, n :int = 1):
+        print(f'n = {n}')
+        for item in range(n):
+            print(item **2 , "is the square of", item)
+
+
 
 
 
 
 
 loop = different_loops()
-#loop.WhileLoop()
+#loop.WhileLoop(4)
+loop.ForLoop(5)
