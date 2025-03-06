635fa244fe9dea0c52f0fe42017a97f6f676da3f
jjosecodes <josejob123121@gmail.com>
2025-03-06 13:50:55 -0600

    done with basic loops notes

--- basics /loops.py
+++ basics /loops.py
@@ -1,3 +1,7 @@
+import math
+
+
+
 # while 
 class different_loops:
     def WhileLoop(self ,counter: int = 1 ):
@@ -11,12 +15,29 @@ class different_loops:
         for item in range(n):
             print(item **2 , "is the square of", item)
 
+    def ForLetters(self):
+        word_list = ["cat", "dog", "rabbit" ]
+        letter_list = []
+        for a_word in word_list:
+            for a_letter in a_word:
+                letter_list.append(a_letter)
+        print(letter_list)
 
 
+# selection statements
+# if else lol
+def IfElse(n: int = 16):
+    if n < 0:
+        print(" value is negative loser , try again ")
+    else:
+        print(math.sqrt(n))
 
-
-
-
+# ------------run loops------------ #
 loop = different_loops()
 #loop.WhileLoop(4)
-loop.ForLoop(5)
+#loop.ForLoop(5)
+#IfElse()
+loop.ForLetters()
+
+# -------------------------- #
+# list comprehension 
